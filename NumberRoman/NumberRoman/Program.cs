466406e15778420ff78321b6afebf072333fab3e
Lazao-Nascimento <lazaronascimentogms@gmail.com>
2025-06-01 21:26:56 -0300

    Organizando Pastas dentro do repositório!

---
+++ NumberRoman/NumberRoman/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int[]? v = TwoSum([2,8,4], 6);
+        
+        if(v != null)
+            System.Console.WriteLine($"{v[0]} {v[1]}");
+    }
+    public static int[]? TwoSum(int[] nums, int target)
+    {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}
