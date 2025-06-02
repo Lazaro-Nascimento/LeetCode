fff26dc4b2ad8c12f3f53067eb58c46f6a9587b8
Lazao-Nascimento <lazaronascimentogms@gmail.com>
2025-06-01 21:38:09 -0300

    [Medium](accepted) - ContainerMostWater

---
+++ ContainerMostWater/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    public int MaxArea(int[] height) {
+        int maior = 0;
+        for(int i=0; i<height.Length; i++)
+        {
+            for(int j=i+1;j<height.Length;j++)
+            {
+                if(height[i] > height[j])
+                {
+                    if(maior < height[i] * (j - i))
+                    {
+                        maior = (j - i) * height[j];
+                    }
+                }
+                else
+                {
+                    if(maior < height[i] * (j - i))
+                    {
+                        maior = (j - i) * height[i];
+                    }
+                }
+            }
+        }
+        return maior;
+    }
+}
